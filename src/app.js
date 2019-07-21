67dfc882787ac8844b534afa4a11c3c7b33058aa
Jenn Gonzalez <jgonz9873@gmail.com>
2019-07-20 17:48:08 -0700

    added routing for card and list requests

--- src/app.js
+++ src/app.js
@@ -4,24 +4,13 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const winston = require('winston')
+// const uuid = require('uuid/v4')
+const cardRouter = require('./card/card-router')
+const listRouter = require('./list/list-router')
+
 
 const app = express()
 
-// set up winston:
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File( { filename: 'info.log '} )
-    ]
-});
-
-if(NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny'
@@ -30,27 +19,30 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-
-
-const cards = [{
-    id: 1,
-    title: 'Task One',
-    content: 'This is card one'
-}];
-
-const lists = [{
-    id: 1,
-    header: 'List One',
-    cardIds: [1]
-}];
+// app.use(express.json());
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res
+                .status(401)
+                .json({ error: 'Unauthorized request' })
+    }
+    next()
 })
 
-app.use(function errorHanlder(error, req, res, next) {
+// put this after the validator middleware, as we want the validation to take place before any routes get handled:
+app.use(cardRouter)
+app.use(listRouter)
+
+
+
+app.use(function errorHandler(error, req, res, next) {
     let response
     if(NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
